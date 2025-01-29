ada81da11248b06c2105a9c06e237a183fc9b82d
jatinag17 <jatinagrawal128@gmail.com>
2025-01-29 14:31:48 +0530

    database setup

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 import express from 'express';
+import "dotenv/config"
 const app = express(); 
-const PORT
+const PORT =process.env.PORT || 8000
+
+app.get("/",(req,res)=>{
+    return res.json({message:"Hello It's working.."});
+
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
